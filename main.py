1dc6fb6e1e5d4a87da912933f521289e655c7957
AEghasemi <a.ehsan.ghasemi@gmail.com>
2021-11-28 18:18:25 -0500

    main.py is updated

--- main.py
+++ main.py
@@ -2,25 +2,51 @@
 # California Housing Prices (Final Project)
 # Team-Avatar
 
-# Importing libraries
+#*************************************************************************************************
+#....> Importing modules
 import numpy as np
 import pandas as pd
-import time
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-# Extract raw data from Data folder:
-data = pd.read_csv('Data/housing.csv')
+import time
 
 
+#....> AI/ML modules
+from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
 
-print('There are {} rows and {} columns in train'.format(data.shape[0],data.shape[1]))
+from sklearn import linear_model
+from sklearn.tree import DecisionTreeRegressor
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error 
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+
+
+#....> Preventing warnings
+import warnings      #....> Prevent from printing the warning of plotting
+warnings.simplefilter(action="ignore", category=FutureWarning)
+#....................................................................................................
+#....................................................................................................
+print('Modules are imported')  #....> If prints, this means that the modules are imported correctly
+#****************************************************************************************************
+
+### Read in all our data
+
+data = pd.read_csv('Data/housing.csv')  #....> Importing dataframe
+rows,columns = df.shape
+print("Dataframe number of rows: ", rows)
+print("Dataframe number of columns: ", columns)
+
+
+
+#print('There are {} rows and {} columns in train'.format(data.shape[0],data.shape[1]))
+
 
 
 #Split the data first and do all feature transformations after the test_train splitting on the train set only to avoid data leakage
-
-from sklearn.model_selection import train_test_split
-
 train_set, test_set = train_test_split(data, test_size=0.2, random_state=1)
 
 print("Training Data", len(train_set))
