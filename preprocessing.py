07402294e050d8a7ad0158f6323b72bb11270f51
AEghasemi <a.ehsan.ghasemi@gmail.com>
2021-11-29 03:17:34 -0500

    preprocessing.py is modified

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,4 @@
-#                      ....> Checking the data structure <....
-
+#....> Checking the data structure 
 def show_structure(df):
     print('\n------------------------------------------------------------------------------------------------------------------------------\n')
     print("Its helpful to see the dataframe structure:\n")
@@ -8,15 +7,15 @@ def show_structure(df):
     print('------------------------------------------------------------------------------------------------------------------------------\n')
     
  
-def add_more_features(df):
+def adding_additional_features(df):
     print('It might be interesting to add the possibly helpful attributes combinations and study their effect on modeling. So:\n')
     df["rooms_per_household"]      = df["total_rooms"]/df["households"]
     df["bedrooms_per_room"]        = df["total_bedrooms"]/df["total_rooms"]
     df["population_per_household"] = df["population"]/df["households"]
-          
+    return df     
 
 
-def handling_categorical(df):
+def handling_categorical_attributes(df):
     print('------------------------------------------------------------------------------------------------------------------------------\n')
     print('Notice: All features are numerical, except the ocean_proximity. Its type is object and its text feature (Categorical feature)\n')
     print('First, we have to explore this categorical feature\n')
@@ -27,18 +26,23 @@ def handling_categorical(df):
     df.loc[df['ocean_proximity'] == '<1H OCEAN', 'ocean_proximity']  = 2
     df.loc[df['ocean_proximity'] == 'INLAND', 'ocean_proximity']     = 1
     df.loc[df['ocean_proximity'] == 'ISLAND', 'ocean_proximity']     = 0
+    return df
 
-
-def splitting_dataset(df):
+def data_splitting(df):
     from sklearn.model_selection import train_test_split
     print("Now the data will be splitted in train and test set.\n")
     train_set1, test_set1 = train_test_split(df, test_size=0.2, random_state=1)
     print("Training Data:", len(train_set1))
     print("Testing Data:", len(test_set1))      
-          
+    return train_set1, test_set1     
     
 
-
+def check_null(dataset):
+    print('------------------------------------------------------------------------------------------------------------------------------')
+    print('Check if there are any missing values (NaN or None)\n')
+    missing_values_count = dataset.isnull().sum()
+    print(missing_values_count[:])
+    
 
 
 
