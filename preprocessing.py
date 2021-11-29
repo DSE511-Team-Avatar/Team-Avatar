05cdb0c90e8743afebfd1e29802fb068ecf28560
AEghasemi <a.ehsan.ghasemi@gmail.com>
2021-11-28 20:06:05 -0500

    preprocessing.py is modified

--- preprocessing.py
+++ preprocessing.py
@@ -3,7 +3,7 @@
 def show_structure(df):
     print('\n------------------------------------------------------------------------------------------------------------------------------\n')
     print("Its helpful to see the dataframe structure:\n")
-    print(df.head())
+    print(df)
     print("\n")
     print('------------------------------------------------------------------------------------------------------------------------------\n')
     
@@ -13,11 +13,10 @@ def add_more_features(df):
     df["rooms_per_household"]      = df["total_rooms"]/df["households"]
     df["bedrooms_per_room"]        = df["total_bedrooms"]/df["total_rooms"]
     df["population_per_household"] = df["population"]/df["households"]
-    print('Our new dataframe has the following columns:\n')
-    print(df.head(n=5))    #....> Show the first 5 rows      
+          
 
 
-def Handling_categorical(df):
+def handling_categorical(df):
     print('------------------------------------------------------------------------------------------------------------------------------\n')
     print('Notice: All features are numerical, except the ocean_proximity. Its type is object and its text feature (Categorical feature)\n')
     print('First, we have to explore this categorical feature\n')
@@ -28,3 +27,21 @@ def Handling_categorical(df):
     df.loc[df['ocean_proximity'] == '<1H OCEAN', 'ocean_proximity']  = 2
     df.loc[df['ocean_proximity'] == 'INLAND', 'ocean_proximity']     = 1
     df.loc[df['ocean_proximity'] == 'ISLAND', 'ocean_proximity']     = 0
+
+
+def splitting_dataset(df):
+    from sklearn.model_selection import train_test_split
+    print("Now the data will be splitted in train and test set.\n")
+    train_set1, test_set1 = train_test_split(df, test_size=0.2, random_state=1)
+    print("Training Data:", len(train_set1))
+    print("Testing Data:", len(test_set1))      
+          
+    
+
+
+
+
+
+
+
+
