29821115d5fd685fc9edcb03ac8f86a615954c55
AEghasemi <a.ehsan.ghasemi@gmail.com>
2021-11-29 03:42:19 -0500

    preprocessing.py is modified

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+
+
 #....> Checking the data structure 
 def show_structure(df):
     print('\n------------------------------------------------------------------------------------------------------------------------------\n')
@@ -45,7 +49,29 @@ def check_null(dataset):
     
 
 
+def missing_percentage(dataset):
+    print('------------------------------------------------------------------------------------------------------------------------------')
+    print('Find the percentage of the missing values in our dataset\n')
+    total_cells          = np.product(dataset.shape)
+    missing_values_count = dataset.isnull().sum()
+    total_missing        = missing_values_count.sum()
+    percent_missing      = (total_missing/total_cells)*100
+    print(percent_missing)
+    
 
-
-
-
+def data_cleaning(train_set, test_set):
+    print('------------------------------------------------------------------------------------------------------------------------------')
+    print('Replacing NAN with Imputer (median) using Scikit-learn\n')
+    from sklearn.impute import SimpleImputer
+    #....> Training Set
+    imputer = SimpleImputer(strategy = "median") #....> Using medain
+    training_numerical_attributes = train_set.drop("ocean_proximity", axis = 1)
+    imputer.fit(training_numerical_attributes)  
+    X1 = imputer.transform(training_numerical_attributes)
+    #....> Testing Set
+    imputer = SimpleImputer(strategy = "median")  #....> Using medain
+    testing_numerical_attributes = test_set.drop("ocean_proximity", axis = 1)
+    imputer.fit(testing_numerical_attributes)  
+    X2 = imputer.transform(testing_numerical_attributes)
+    print("Both Training and Testing sets the NAN values are replaced with medain")
+    return X1, X2
