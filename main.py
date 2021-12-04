f4b29949b5bb9c92f3d1338663e53ac7d397f16d
AJ311021 <93494354+AJ311021@users.noreply.github.com>
2021-12-04 09:51:49 -0500

    add (import matplotlib as mpl)

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 #....> Preventing warnings
 import warnings      #....> Prevent from printing the warning of plotting
