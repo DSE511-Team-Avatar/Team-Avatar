ffcb216773ba354830a214519677019ab8f60a68
AEghasemi <a.ehsan.ghasemi@gmail.com>
2021-12-05 05:13:30 -0500

    main.py is finished with preprcessing

--- src/main.py
+++ src/main.py
@@ -17,62 +17,23 @@ warnings.simplefilter(action="ignore", category=FutureWarning)
 
 print('Modules in main.py are imported\n') #....> If prints, this means that the modules in main.py are imported correctly
 
+#****************************************************************************************************
+#                                 Insert dataframe
+#****************************************************************************************************
 
-
+df = pd.read_csv('../Data/housing.csv')  #....> Importing dataframe
 
 
 #****************************************************************************************************
+#                                Data pre-processing 
 #****************************************************************************************************
-#                         ....> Read dataset <....
-
-df = pd.read_csv('Data/housing.csv')  #....> Importing dataframe
-rows, columns = df.shape
-print("Dataframe number of rows: ", rows)
-print("Dataframe number of columns: ", columns)
-
-#****************************************************************************************************
-#****************************************************************************************************
-#                         ....> Data pre-processing <....
-
 #....> Import pre-processing module
 from preprocessing import *
 
+housing, housing_labels, housing_t, housing_labels_t = preprocessing(df)
 
-#....> Show the structure
-show_structure(df)
-
-#....> Add some more features
-df = adding_additional_features(df)
-show_structure(df)
-
-#...> Handling categorical feature(ocean_proximity)
-df = handling_categorical_attributes(df)
-show_structure(df)
-
-#....> Data Splitting
-train_set, test_set = data_splitting(df)
-
-# Check for NaN
-#....> Check the train_set
-check_null(train_set)
-#....> Check the test_set
-check_null(test_set)
-
-
-# Check the Percentage of miising value
-#....> Check the train_set
-missing_percentage(train_set)
-#....> Check the test_set
-missing_percentage(test_set)
-
-
-# Data cleaning 
-train_set, test_set = data_cleaning(df,train_set, test_set)
-
-# show_structure(df)
-show_structure(train_set)
-show_structure(test_set)
-
-
-
+    
+#****************************************************************************************************
+#                                Data pre-processing 
+#****************************************************************************************************
 
